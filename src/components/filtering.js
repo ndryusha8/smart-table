592b44295c996c57e47573d47b087a1097c10a1f
Andrey Yudin <ndryusha8@yandex.ru>
2026-01-04 19:08:19 +0300

    Добавление фильтра через сервер, удаление compare.js

--- src/components/filtering.js
+++ src/components/filtering.js
@@ -1,22 +1,16 @@
-import {createComparison, defaultRules} from "../lib/compare.js";
+export function initFiltering(elements) {
+    const updateIndexes = (elements, indexes) => {
+        Object.keys(indexes).forEach((elementName) => {
+            elements[elementName].append(...Object.values(indexes[elementName]).map(name => {
+                const el = document.createElement('option');
+                el.textContent = name;
+                el.value = name;
+                return el;
+            }))
+        })
+    }
 
-export function initFiltering(elements, indexes) {
-    Object.keys(indexes)                                    // Получаем ключи из объекта
-          .forEach((elementName) => {                        // Перебираем по именам
-            elements[elementName].append(                    // в каждый элемент добавляем опции
-                ...Object.values(indexes[elementName])        // формируем массив имён, значений опций
-                          .map(name => {                        // используйте name как значение и текстовое содержимое
-                            const option = document.createElement('option');
-                            option.value = name;
-                            option.textContent = name;
-                            return option;
-                          })
-            )
-         });
-
-    const compare = createComparison(defaultRules);
-
-    return (data, state, action) => {
+    const applyFiltering = (query, state, action) => {
         if (action && action.name === 'clear') {
             const input = action.parentElement.querySelector('input');
             if (input) {
@@ -25,6 +19,21 @@ export function initFiltering(elements, indexes) {
             }
         }
 
-        return data.filter(row => compare(row, state));
+        // @todo: #4.5 — отфильтровать данные, используя компаратор
+        const filter = {};
+        Object.keys(elements).forEach(key => {
+            if (elements[key]) {
+                if (['INPUT', 'SELECT'].includes(elements[key].tagName) && elements[key].value) { // ищем поля ввода в фильтре с непустыми данными
+                    filter[`filter[${elements[key].name}]`] = elements[key].value; // чтобы сформировать в query вложенный объект фильтра
+                }
+            }
+        })
+
+        return Object.keys(filter).length ? Object.assign({}, query, filter) : query; // если в фильтре что-то добавилось, применим к запросу
+    }
+
+    return {
+        updateIndexes,
+        applyFiltering
     }
 }
